756cb4a1689d864f3a2dd0af885a201a25861759
divyanshu98163 <divy9anshu>
2024-06-19 08:35:18 +0530

    new class pen and student

---
+++ OPPS.java
@@ -0,0 +1,30 @@
+public class OPPS{
+        public static void main(String[] args) {
+            Pen p1 = new Pen();
+            p1.Setcolor("red");
+            System.out.println(p1.Color);
+            p1.SetTip(5);
+            System.out.println(p1.Tip);
+
+        }
+}
+class Pen{
+    String Color;
+    int Tip;
+
+    void Setcolor(String newColor){
+        Color = newColor;
+    }
+
+    void SetTip(int newTip){
+        Tip = newTip;
+    }
+}
+class Student{
+    String name;
+    int age;
+    float percntage;
+    void CalcuParcentage(int phy,int chem,int math){
+        percntage = (phy+chem+math)/3;
+    }
+}
