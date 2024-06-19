11834b96e18cee3438135c29e4ffa92ffde896d3
divyanshu98163 <divy9anshu>
2024-06-19 17:06:02 +0530

    Bank account prive member is not acces

---
+++ Oops2.java
@@ -0,0 +1,20 @@
+
+
+public class Oops2 {
+    public static void main(String args[]){
+        BankAccount myAc = new BankAccount();
+        myAc.username ="Divyanshu";
+        System.out.println(myAc.username);
+        myAc.Setpassword("yguguy");
+        
+    }
+
+}
+class BankAccount{
+    public String username;
+    private String Password;
+    public void Setpassword(String pwd){
+        Password = pwd;
+    }
+
+}
